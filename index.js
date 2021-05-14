fec230e7584266ef9f424d7e3c769b9d97fa050c
Pedro <pedro_citadin@outlook.com>
2021-05-14 14:22:38 -0300

    Versão 2.0 com filtro por liga

--- index.js
+++ index.js
@@ -11,8 +11,46 @@ app.use("/topvalores", function (req, res) {
     var jogadores = []
     const sq = (req.query).liga
     if (verificaFiltro(sq)) {
-        console.log(retornaURL(sq))
+        const url = retornaURL(sq)
+        var requisicao = axios.get(url)
+        requisicao.then(function (resposta) {
+            var root = parser(resposta.data)
+            var rt = root.querySelectorAll(".items")[0]['childNodes'][3]['childNodes']
+            var r2 = []
+            for (let i = 0; i < rt.length; i++) {
+                if (i % 2 == 0) {
 
+                } else {
+                    r2.push(rt[i])
+                }
+            }
+            for (let i = 0; i < r2.length; i++) {
+                var nomep = {
+                    nome: r2[i]['childNodes'][2]['childNodes'][1]['childNodes'][1]['childNodes'][3]['childNodes'][1]['_attrs']['title'],
+                    posicao: r2[i]['childNodes'][2]['childNodes'][1]['childNodes'][3]['childNodes'][1]['childNodes'][0]['rawText'],
+                    idade: r2[i]['childNodes'][4]['childNodes'][0]['rawText'],
+                    nacionalidade: r2[i]['childNodes'][3]['childNodes'][0]['_attrs']['alt'],
+                    clube: r2[i]['childNodes'][5]['childNodes'][0]['childNodes'][0]["_attrs"]['alt'],
+                    valor_mercado: r2[i]['childNodes'][6]['childNodes'][0]['childNodes'][0]['rawText'],
+                    url_imagem: r2[i]['childNodes'][2]['childNodes'][1]['childNodes'][1]['childNodes'][1]['childNodes'][1]['_attrs']['src']
+                }
+                jogadores.push(nomep)
+            }
+    
+            res.send(jogadores) 
+         
+                      
+
+
+
+
+
+
+
+
+
+
+        })
 
 
 
