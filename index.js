db2fa748273047c1bc83862c545469aa6359ec06
Pedro <pedro_citadin@outlook.com>
2021-05-17 15:52:38 -0300

    Final Final

--- index.js
+++ index.js
@@ -12,35 +12,35 @@ app.use("/topjogadores", function (req, res) {
     const sq = (req.query).liga
     if (verificaFiltro(sq)) {
         const url = retornaURL(sq)
-        if(url == false){
+        if (url == false) {
             res.send("Liga não encontrada!")
-        }else{
-        var requisicao = axios.get(url)
-        requisicao.then(function (resposta) {
-            var root = parser(resposta.data)
-            var rt = root.querySelectorAll(".items")[0]['childNodes'][3]['childNodes']
-            var r2 = []
-            for (let i = 0; i < rt.length; i++) {
-                if (i % 2 == 0) {
+        } else {
+            var requisicao = axios.get(url)
+            requisicao.then(function (resposta) {
+                var root = parser(resposta.data)
+                var rt = root.querySelectorAll(".items")[0]['childNodes'][3]['childNodes']
+                var r2 = []
+                for (let i = 0; i < rt.length; i++) {
+                    if (i % 2 == 0) {
 
-                } else {
-                    r2.push(rt[i])
+                    } else {
+                        r2.push(rt[i])
+                    }
                 }
-            }
-            for (let i = 0; i < r2.length; i++) {
-                var nomep = {
-                    nome: r2[i]['childNodes'][2]['childNodes'][1]['childNodes'][1]['childNodes'][3]['childNodes'][1]['_attrs']['title'],
-                    posicao: r2[i]['childNodes'][2]['childNodes'][1]['childNodes'][3]['childNodes'][1]['childNodes'][0]['rawText'],
-                    idade: r2[i]['childNodes'][4]['childNodes'][0]['rawText'],
-                    nacionalidade: r2[i]['childNodes'][3]['childNodes'][0]['_attrs']['alt'],
-                    clube: r2[i]['childNodes'][5]['childNodes'][0]['childNodes'][0]["_attrs"]['alt'],
-                    valor_mercado: r2[i]['childNodes'][6]['childNodes'][0]['childNodes'][0]['rawText'],
-                    url_imagem: r2[i]['childNodes'][2]['childNodes'][1]['childNodes'][1]['childNodes'][1]['childNodes'][1]['_attrs']['src']
+                for (let i = 0; i < r2.length; i++) {
+                    var nomep = {
+                        nome: r2[i]['childNodes'][2]['childNodes'][1]['childNodes'][1]['childNodes'][3]['childNodes'][1]['_attrs']['title'],
+                        posicao: r2[i]['childNodes'][2]['childNodes'][1]['childNodes'][3]['childNodes'][1]['childNodes'][0]['rawText'],
+                        idade: r2[i]['childNodes'][4]['childNodes'][0]['rawText'],
+                        nacionalidade: r2[i]['childNodes'][3]['childNodes'][0]['_attrs']['alt'],
+                        clube: r2[i]['childNodes'][5]['childNodes'][0]['childNodes'][0]["_attrs"]['alt'],
+                        valor_mercado: r2[i]['childNodes'][6]['childNodes'][0]['childNodes'][0]['rawText'],
+                        url_imagem: r2[i]['childNodes'][2]['childNodes'][1]['childNodes'][1]['childNodes'][1]['childNodes'][1]['_attrs']['src']
+                    }
+                    jogadores.push(nomep)
                 }
-                jogadores.push(nomep)
-            }
 
-            res.send(jogadores)
+                res.send(jogadores)
 
 
 
@@ -53,9 +53,9 @@ app.use("/topjogadores", function (req, res) {
 
 
 
-        })
+            })
 
-    }
+        }
 
 
 
@@ -114,41 +114,41 @@ app.use("/topjogadores", function (req, res) {
 app.use("/topclubes", function (req, res) {
     var clubes = []
     const sq = (req.query).confederacao
-    
+
     if (verificaFiltro(sq)) {
-    const url = retornaURLFed(sq)
-    if(url == false){
-        res.send("Confederação não encontrada!")
-    }else{
-    var requisicao = axios.get(url)
-        requisicao.then(function (resposta) {
-            var root = parser(resposta.data)
-            var rt = root.querySelectorAll('.items')[0]['childNodes'][3]['childNodes']
-            var r2 = []
-            for (let i = 0; i < rt.length; i++) {
-                if (i % 2 == 0) {
+        const url = retornaURLFed(sq)
+        if (url == false) {
+            res.send("Confederação não encontrada!")
+        } else {
+            var requisicao = axios.get(url)
+            requisicao.then(function (resposta) {
+                var root = parser(resposta.data)
+                var rt = root.querySelectorAll('.items')[0]['childNodes'][3]['childNodes']
+                var r2 = []
+                for (let i = 0; i < rt.length; i++) {
+                    if (i % 2 == 0) {
 
-                } else {
-                    r2.push(rt[i])
+                    } else {
+                        r2.push(rt[i])
+                    }
                 }
-            }
 
-            for (let i = 0; i < r2.length; i++) {
-                var nomep = {
-                    nome: r2[i]['childNodes'][2]['childNodes'][0]['childNodes'][0]['_attrs']['alt'],
-                    pais: r2[i]['childNodes'][4]['childNodes'][0]['_attrs']['alt'],
-                    liga: r2[i]['childNodes'][4]['childNodes'][2]['_attrs']['title'],
-                    valor_mercado: r2[i]['childNodes'][5]['childNodes'][0]['childNodes'][0]['rawText'],
-                    url_imagem: r2[i]['childNodes'][2]['childNodes'][0]['childNodes'][0]['_attrs']['src']
-                   
+                for (let i = 0; i < r2.length; i++) {
+                    var nomep = {
+                        nome: r2[i]['childNodes'][2]['childNodes'][0]['childNodes'][0]['_attrs']['alt'],
+                        pais: r2[i]['childNodes'][4]['childNodes'][0]['_attrs']['alt'],
+                        liga: r2[i]['childNodes'][4]['childNodes'][2]['_attrs']['title'],
+                        valor_mercado: r2[i]['childNodes'][5]['childNodes'][0]['childNodes'][0]['rawText'],
+                        url_imagem: r2[i]['childNodes'][2]['childNodes'][0]['childNodes'][0]['_attrs']['src']
+
+                    }
+                    clubes.push(nomep)
                 }
-                clubes.push(nomep)
-            }
-           res.send(clubes)
+                res.send(clubes)
 
 
-        })
-    }
+            })
+        }
 
     } else {
         const url = dados.confederacoes[0].local
@@ -172,11 +172,11 @@ app.use("/topclubes", function (req, res) {
                     liga: r2[i]['childNodes'][4]['childNodes'][2]['_attrs']['title'],
                     valor_mercado: r2[i]['childNodes'][5]['childNodes'][0]['childNodes'][0]['rawText'],
                     url_imagem: r2[i]['childNodes'][2]['childNodes'][0]['childNodes'][0]['_attrs']['src']
-                   
+
                 }
                 clubes.push(nomep)
             }
-           res.send(clubes)
+            res.send(clubes)
 
 
         })
@@ -186,7 +186,7 @@ app.use("/topclubes", function (req, res) {
 })
 
 
-app.use("/autor", function(req, res){
+app.use("/autor", function (req, res) {
     const dados = require("./api/info.json")
     res.send(dados)
 })
@@ -215,21 +215,21 @@ function retornaURL(qs) {
         return dados.campeonato[1].local
     } else if (qs.toUpperCase() == "laliga".toUpperCase()) {
         return dados.campeonato[2].local
-    } else if (qs.toUpperCase()== "ligue1".toUpperCase()) {
+    } else if (qs.toUpperCase() == "ligue1".toUpperCase()) {
         return dados.campeonato[3].local
     } else if (qs.toUpperCase() == "seriea".toUpperCase()) {
         return dados.campeonato[4].local
     } else if (qs.toUpperCase() == "bundesliga".toUpperCase()) {
         return dados.campeonato[5].local
-    }else{
+    } else {
         return false
     }
 }
 
-function retornaURLFed(qs){
-    for(let i = 0; i<dados.confederacoes.length; i++){
-        if(qs.toUpperCase()==dados.confederacoes[i].nome.toUpperCase()){
-            return dados.confederacoes[i].local 
+function retornaURLFed(qs) {
+    for (let i = 0; i < dados.confederacoes.length; i++) {
+        if (qs.toUpperCase() == dados.confederacoes[i].nome.toUpperCase()) {
+            return dados.confederacoes[i].local
         }
     }
     return false
